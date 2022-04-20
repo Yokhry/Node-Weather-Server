bf9e1c79aa91c5543bbaaf8528818a17303fac45
Pratham Baliyan <prathambaliyan012@gmail.com>
2022-04-20 14:05:42 +0530

    init commit

---
+++ src/app.js
@@ -0,0 +1,112 @@
+const request = require('request')
+const path = require('path')
+const express = require('express')
+const hbs = require('hbs')
+const geocode = require('./utils/geocode')
+const forecast = require('./utils/forecast')
+
+
+const app = express()
+
+//define paths for express config
+const pathtodirectory = path.join(__dirname,'../public')
+const viewsPath = path.join(__dirname,'../template/views')
+const partialsPath = path.join(__dirname,'../template/partials')
+
+//setup handlebars engine and views
+app.set('view engine','hbs')
+app.set('views',viewsPath)
+hbs.registerPartials(partialsPath)
+
+//setup static directory to serve
+app.use(express.static(pathtodirectory))
+app.use(express.static(viewsPath))
+
+app.get('',(req,res)=>{
+    res.render('index',{
+        title: 'Hello There Todays Weather Is ',
+       
+        Name: 'Pratham Baliyan'
+    })
+})
+
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title: 'Notice',
+       
+        Name: 'Pratham Baliyan'
+       
+    })
+})
+
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        HelpText: 'Need Help for Needy',
+        title: 'Help',
+        Name: 'Pratham Baliyan'
+    })
+})
+//.get() this help us configure what the servers should do when someone wants to get something
+//.send() allows us to send something to the requestor
+
+
+app.get('/weather',(req,res)=>{
+    if(!req.query.address){
+        return res.send({
+            Error:'Please Provide A Address'
+        })
+    }
+
+    geocode(req.query.address,(error,{lattitude,longitude,location} = {})=>{
+        if(error){
+            return res.send({error})
+        }
+        forecast(lattitude,longitude,(error,forecastData)=>{
+            if(error){
+                return res.send({error})
+            }
+            res.send({
+                forecast: forecastData,
+                location,
+                address: req.query.address
+            })
+        })
+    })
+        
+      })
+   
+    
+
+
+app.get('/products',(req,res)=>{
+    if(!req.query.search){
+        return res.send({
+            Error: 'Search Bar Cannot Be Empty'
+        })
+    }
+    console.log(req.query.search)
+    res.send({
+        products: []
+    })
+})
+
+app.get('/help/*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        Name: 'Pratham Baliyan',
+        ErrorMessage: 'No further Page'
+    })
+})
+
+app.get('/*',(req,res)=>{
+    res.render('404',{
+        title: '404',
+        Name: 'Pratham Baliyan',
+        ErrorMessage: 'Page Not Found'
+    })
+})
+
+
+app.listen(3000, ()=>{
+    console.log('server is up on port 3000.')
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,24 @@
+const request = require('request')
+
+
+
+const forecast = (lattitude,longitude,callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=f9953132f022ca664f7bf498e5df86e1&query=' + lattitude +',' + longitude + '&units=m'
+    request({url,json:true},(error,{ body }) => {
+        if(error) {
+            callback('Unable to connect to loaction',undefined)
+        }
+        else if (body.error ){
+            callback('Unable to find the location, Try Another search',undefined)
+        }
+        else {
+            callback(undefined,body.current.weather_descriptions + '. It Feels Like  ' + body.current.feelslike + '° Celcius , But its '+ body.current.temperature + '° Celcius Out There' 
+                
+            )
+                
+        }
+    })
+
+}
+
+module.exports = forecast
